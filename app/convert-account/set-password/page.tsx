84f80177d52459a2ea9ccbfd12c7c7f00361b10d
yoheisuda <y1405705@gmail.com>
2024-10-07 00:01:57 +0900

    add auth

---
+++ app/convert-account/set-password/page.tsx
@@ -0,0 +1,34 @@
+import { resetPasswordAction } from "@/app/actions";
+import { FormMessage } from "@/components/form-message";
+import { SubmitButton } from "@/components/submit-button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+export default function SetPassword({ searchParams }: { searchParams: { message?: string } }) {
+  return (
+    <form className="flex flex-col w-full max-w-md p-4 gap-2 [&>input]:mb-4">
+      <h1 className="text-2xl font-medium">パスワードを設定してください</h1>
+      <p className="text-sm text-foreground/60">
+        セキュリティのため、新しいパスワードを設定してください。
+      </p>
+      <Label htmlFor="password">新しいパスワード</Label>
+      <Input
+        type="password"
+        name="password"
+        placeholder="新しいパスワード"
+        required
+      />
+      <Label htmlFor="confirmPassword">パスワードの確認</Label>
+      <Input
+        type="password"
+        name="confirmPassword"
+        placeholder="パスワードの確認"
+        required
+      />
+      <SubmitButton formAction={resetPasswordAction}>
+        パスワードを設定
+      </SubmitButton>
+      {searchParams.message && <FormMessage message={{ error: searchParams.message }} />}
+    </form>
+  );
+}
--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,7 +1,8 @@
-import FetchDataSteps from "@/components/tutorial/fetch-data-steps";
 import { createClient } from "@/utils/supabase/server";
 import { InfoIcon } from "lucide-react";
 import { redirect } from "next/navigation";
+import SampleTable from "@/components/sample-table/sample-table";
+import Link from "next/link";
 
 export default async function ProtectedPage() {
   const supabase = createClient();
@@ -14,10 +15,26 @@ export default async function ProtectedPage() {
     return redirect("/sign-in");
   }
 
+  const isAnonymous = user.is_anonymous
+
   return (
     <div className="flex-1 w-full flex flex-col gap-12">
-      <div className="w-full">
-        <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
+      {isAnonymous && (
+        <div className="flex justify-center">
+          <Link href="/convert-account" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          本アカウント登録
+          </Link>
+        </div>
+      )}
+      {!isAnonymous && (
+        <div className="flex justify-center">
+        <Link href="/convert-account/set-password" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        パスワードを設定/更新する
+        </Link>
+      </div>
+      )}
+        <div className="w-full">
+          <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
           <InfoIcon size="16" strokeWidth={2} />
           This is a protected page that you can only see as an authenticated
           user
@@ -29,10 +46,7 @@ export default async function ProtectedPage() {
           {JSON.stringify(user, null, 2)}
         </pre>
       </div>
-      <div>
-        <h2 className="font-bold text-2xl mb-4">Next steps</h2>
-        <FetchDataSteps />
-      </div>
+      <SampleTable />
     </div>
   );
 }
